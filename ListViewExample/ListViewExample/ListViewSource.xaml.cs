bdd6a0867520c4067797998da8046d4af46c2516
Ventura <marlenp363@gmail.com>
2022-04-27 12:27:19 -0500

    primer commit

---
+++ ListViewExample/ListViewExample/ListViewSource.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewSource : ContentPage
+    {
+        ObservableCollection<Employe> employes = new ObservableCollection<Employe>();
+        
+        public ObservableCollection<Employe> Employes { get { return employes; } }
+        public ListViewSource()
+        {
+            InitializeComponent();
+
+            EmployeeView.ItemsSource = employes;
+            employes.Add(new Employe { DisplayName = "Rob Finnerty" });
+            employes.Add(new Employe { DisplayName = "Bill Wrestler" });
+            employes.Add(new Employe { DisplayName = "Dr. Geri-Beth Hooper" });
+            employes.Add(new Employe { DisplayName = "Dr. Keith Joyce-Purdy" });
+            employes.Add(new Employe { DisplayName = "Sheri Spruce" });
+            employes.Add(new Employe { DisplayName = "Burt Indybrick" });
+        }
+    }
+}
